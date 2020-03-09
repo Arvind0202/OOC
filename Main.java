bb42bd34e76a4003f428960b81f1242d02c1422d
manjuvino <55084950+manjuvino@users.noreply.github.com>
2020-03-09 16:31:24 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.awt.*;
+
+import java.awt.event.*;
+
+import java.applet.*;
+
+/*<applet code="Main" width =300 height=300></applet>*/
+
+public class Main extends Applet implements MouseListener
+
+{
+    String msg="hj";
+    
+int mx=0,my=0;
+    
+public void init()
+    
+{
+        
+addMouseListener(this);
+    
+}
+    
+public void mousePressed(MouseEvent m)
+    
+{
+        
+mx=m.getX();
+        my=m.getY();
+        msg="hai";
+        
+repaint();
+            }
+            
+public void paint(Graphics g)
+            
+{
+                g.drawString(msg,mx,my);
+            
+}
+}
